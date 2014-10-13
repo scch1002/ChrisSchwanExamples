01f5306e6ead4b49e4faaf4c4996dd82f7b4f564
scch1002 <chrisschwan8009@hotmail.com>
2014-10-12 23:51:48 -0500

    Added google maps

--- AngularMetaApp/app/droppin/droppin.js
+++ AngularMetaApp/app/droppin/droppin.js
@@ -7,12 +7,19 @@
         var getLogFn = common.logger.getLogFn;
         var log = getLogFn(controllerId);
 
+        var mapOptions = {
+            center: { lat: -34.397, lng: 150.644},
+            zoom: 8
+        };
+
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+    
         activate();
 
         function activate() {
             common.activateController(controllerId)
-        }
 
-        $scope.check = 'This is a check.';
+        }
     }
 })();
